a4ebf89193ac86f351ff15c9a0461ecec13c1282
Periasamy147 <postbox1472@gmail.com>
2025-10-15 12:33:28 +0530

    Update Selenium_Helper to match with the app_DB

--- testing/selenium_helper.py
+++ testing/selenium_helper.py
@@ -23,7 +23,9 @@ def init_test_db():
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             username VARCHAR(50) UNIQUE NOT NULL,
-            password TEXT NOT NULL
+            password TEXT NOT NULL,
+            name VARCHAR(100),
+            age INT
         );
     """)
     conn.commit()
